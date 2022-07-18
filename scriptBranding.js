3d98ebf4d76e6034821a68deb2f75fc640f99d85
aergesrg <74409210+DmitriyKwelov@users.noreply.github.com>
2022-07-18 23:21:47 +0300

    1

--- scriptBranding.js
+++ scriptBranding.js
@@ -4,7 +4,6 @@ const switchInfoItem = document.querySelectorAll('.switch__info__item')
 const switchableContent = document.querySelectorAll('.switchable__content')
 const container = document.querySelector('.container')
 const contentWidts = parseInt(getComputedStyle(container).width);
-console.log(contentWidts);
 
 if (window.screen.width > 992) {
     switchInfoItem.forEach(item => {
@@ -32,14 +31,13 @@ if (window.screen.width > 992) {
     }
 }
 if (window.screen.width > 992) {
-    const trackSwitchInfo = document.querySelector('.switch__info__track');
-    const switchTrack = document.querySelector('.switch__info__track')
+    const switchTrack = document.querySelector('.switch__info__track');
     const switchInfoItemm = document.querySelector('.switch__info__item')
     const widthTrack = parseInt(getComputedStyle(switchInfoItemm).width) + parseInt(getComputedStyle(switchInfoItemm).marginRight);
 
 
     const positionSwitchInfoDesk = parseInt(getComputedStyle(container).marginLeft) + 40;
-    trackSwitchInfo.style.transform = `translateX(${positionSwitchInfoDesk}px)`;
+    switchTrack.style.transform = `translateX(${positionSwitchInfoDesk}px)`;
     let positionNumber = positionSwitchInfoDesk;
     switchTrack.onmousedown = function (event) {
         event.preventDefault()
@@ -50,7 +48,7 @@ if (window.screen.width > 992) {
             finalPosition = positionNumber - position / 25;
             if (positionSwitchInfoDesk >= finalPosition && -((widthTrack * 6) - window.screen.width) < finalPosition) {
                 positionNumber = finalPosition
-                trackSwitchInfo.style.transform = `translateX(${finalPosition}px)`;
+                switchTrack.style.transform = `translateX(${finalPosition}px)`;
             }
         }
 
@@ -65,7 +63,7 @@ if (window.screen.width > 992) {
 
 if (window.screen.width <= 992) {
     const wraperSwitchInfo = document.querySelector('.wrapper__switch__info');
-    const trackSwitchInfo = document.querySelector('.switch__info__track');
+    const switchTrack = document.querySelector('.switch__info__track');
     const sliderItemSwitchInfo = document.querySelectorAll('.switch__info__item')
     const pointsSliderSwitchInfo = document.querySelector('.points__switch__info')
     const sliderItemSwitchInfoLength = sliderItemSwitchInfo.length
@@ -118,7 +116,7 @@ if (window.screen.width <= 992) {
         }
     }
     const setpositionSwitchInfo = () => {
-        trackSwitchInfo.style.transform = `translateX(${positionSwitchInfo}px)`;
+        switchTrack.style.transform = `translateX(${positionSwitchInfo}px)`;
     }
     const points = document.querySelectorAll('.point__switch__info')
     points.forEach(point => {
@@ -127,7 +125,7 @@ if (window.screen.width <= 992) {
                 if (points[i] == point) {
                     positionSwitchInfo = -(widthSliderSwitchInfo * i)
                     activeSlideSwitchInfo = i
-                    trackSwitchInfo.style.transform = `translateX(${positionSwitchInfo}px)`;
+                    switchTrack.style.transform = `translateX(${positionSwitchInfo}px)`;
                     setPointSwitchInfo();
                 }
             }
@@ -234,7 +232,6 @@ if (window.screen.width <= 992) {
     sliderItemGradien.forEach(item => {
         item.style.minWidth = `${contentWidts}px`
     });
-    console.log(wraperGradien);
     wraperGradien.addEventListener('swiped-right', function (e) {
         if (positionGradien !== 0) {
             positionGradien += widthSliderGradien;
@@ -276,7 +273,7 @@ if (window.screen.width <= 992) {
     }
     const pointsGradien = document.querySelectorAll('.point__gradient__box')
     pointsGradien.forEach(pointGradien => {
-        point1.addEventListener("click", function (e) {
+        pointGradien.addEventListener("click", function (e) {
             for (let i = 0; i < pointsGradien.length; i++) {
                 if (pointsGradien[i] == pointGradien) {
                     positionGradien = -(widthSliderGradien * i)
@@ -288,4 +285,175 @@ if (window.screen.width <= 992) {
         })
     });
 
+}
+
+if (window.screen.width <= 992) {
+    let activeSlideGradien2 = 0;
+    let positionGradien2 = 0;
+    const wraperGradien2 = document.querySelector('.wraper__gradient__box__platform');
+    const trackGradien2 = document.querySelector('.platform__strategy__gradient__box');
+    const sliderItemGradien2 = document.querySelectorAll('.gradient__box__item__platform')
+    const pointsSliderGradien2 = document.querySelector('.poinst__gradient__platform')
+    const sliderItemLengthGradien2 = sliderItemGradien2.length
+    const widthSliderGradien2 = contentWidts
+    const countItemGradien2 = sliderItemGradien2.length
+    sliderItemGradien2.forEach(item => {
+        item.style.minWidth = `${contentWidts}px`
+    });
+    wraperGradien2.addEventListener('swiped-right', function (e) {
+        if (positionGradien2 !== 0) {
+            positionGradien2 += widthSliderGradien2;
+            activeSlideGradien2 = activeSlideGradien2 - 1;
+            setpositionGradien2();
+            setPointGradien2();
+        }
+    })
+
+    wraperGradien2.addEventListener('swiped-left', function (e) {
+        if (positionGradien2 > -(countItemGradien2 * widthSliderGradien2 - widthSliderGradien2)) {
+            positionGradien2 -= widthSliderGradien2;
+            activeSlideGradien2 = activeSlideGradien2 + 1;
+            setpositionGradien2();
+            setPointGradien2();
+        }
+    })
+
+    for (let i = 0; i < sliderItemLengthGradien2; i++) {
+        const span = document.createElement("span");
+        span.className = "point__gradient__platform";
+        span.classList.add('point')
+        if (activeSlideGradien2 == i) {
+            span.classList.add('active');
+        }
+        pointsSliderGradien2.append(span);
+    }
+    const setPointGradien2 = () => {
+        const pointsGradien2 = document.querySelectorAll('.point__gradient__platform')
+        for (let i = 0; i < pointsGradien2.length; i++) {
+            pointsGradien2[i].classList.remove('active');
+            if (i == activeSlideGradien2) {
+                pointsGradien2[i].classList.add('active');
+            }
+        }
+    }
+    const setpositionGradien2 = () => {
+        trackGradien2.style.transform = `translateX(${positionGradien2}px)`;
+    }
+    const pointsGradien2 = document.querySelectorAll('.point__gradient__platform')
+    pointsGradien2.forEach(pointGradien2 => {
+        pointGradien2.addEventListener("click", function (e) {
+            for (let i = 0; i < pointsGradien2.length; i++) {
+                if (pointsGradien2[i] == pointGradien2) {
+                    positionGradien2 = -(widthSliderGradien2 * i)
+                    activeSlideGradien2 = i
+                    trackGradien2.style.transform = `translateX(${positionGradien2}px)`;
+                    setPointGradien2();
+                }
+            }
+        })
+    });
+}
+if (window.screen.width > 992) {
+    const violetTrack = document.querySelector('.violet__box__track');
+    const violetItemm = document.querySelectorAll('.violet__box__item')
+    const violetWidthTrack = parseInt(getComputedStyle(violetItemm[0]).minWidth) + parseInt(getComputedStyle(violetItemm[0]).marginRight);
+
+    violetItemm.forEach(item => {
+        item.addEventListener('click', function (e) {
+            violetItemm.forEach(item => {
+                item.classList.remove('active')
+            });
+            item.classList.add('active')
+        })
+    });
+
+    const positionvioletDesk = parseInt(getComputedStyle(container).marginLeft) + 40;
+    violetTrack.style.transform = `translateX(${positionvioletDesk}px)`;
+    let positionNumber = positionvioletDesk;
+    violetTrack.onmousedown = function (event) {
+        event.preventDefault()
+        const firstPoistX = event.clientX;
+        let finalPosition = null;
+        function onMouseMove(event) {
+            let position = firstPoistX - event.clientX;
+            finalPosition = positionNumber - position / 25;
+            if (positionvioletDesk >= finalPosition && -((violetWidthTrack * violetItemm.length) - window.screen.width) < finalPosition) {
+                positionNumber = finalPosition
+                violetTrack.style.transform = `translateX(${finalPosition}px)`;
+            }
+        }
+
+        violetTrack.addEventListener('mousemove', onMouseMove);
+
+        document.onmouseup = function () {
+            violetTrack.removeEventListener('mousemove', onMouseMove);
+            violetTrack.onmouseup = null;
+        };
+    }
+}
+if (window.screen.width <= 992) {
+    let activeSlideViolet = 0;
+    let positionViolet = 0;
+    const wraperViolet = document.querySelector('.wrapper__slider__scroll');
+    const trackViolet = document.querySelector('.violet__box__track');
+    const sliderItemViolet = document.querySelectorAll('.violet__box__item__scroll')
+    const pointsSliderViolet = document.querySelector('.poinst__violet')
+    const sliderItemLengthViolet = sliderItemViolet.length
+    const widthSliderViolet = contentWidts
+    const countItemViolet = sliderItemViolet.length
+    sliderItemViolet.forEach(item => {
+        item.style.minWidth = `${contentWidts}px`
+    });
+    wraperViolet.addEventListener('swiped-right', function (e) {
+        if (positionViolet !== 0) {
+            positionViolet += widthSliderViolet;
+            activeSlideViolet = activeSlideViolet - 1;
+            setpositionViolet();
+            setPointViolet();
+        }
+    })
+
+    wraperViolet.addEventListener('swiped-left', function (e) {
+        if (positionViolet > -(countItemViolet * widthSliderViolet - widthSliderViolet)) {
+            positionViolet -= widthSliderViolet;
+            activeSlideViolet = activeSlideViolet + 1;
+            setpositionViolet();
+            setPointViolet();
+        }
+    })
+
+    for (let i = 0; i < sliderItemLengthViolet; i++) {
+        const span = document.createElement("span");
+        span.className = "poins__violet";
+        span.classList.add('point')
+        if (activeSlideViolet == i) {
+            span.classList.add('active');
+        }
+        pointsSliderViolet.append(span);
+    }
+    const setPointViolet = () => {
+        const pointsViolet = document.querySelectorAll('.poins__violet')
+        for (let i = 0; i < pointsViolet.length; i++) {
+            pointsViolet[i].classList.remove('active');
+            if (i == activeSlideViolet) {
+                pointsViolet[i].classList.add('active');
+            }
+        }
+    }
+    const setpositionViolet = () => {
+        trackViolet.style.transform = `translateX(${positionViolet}px)`;
+    }
+    const pointsViolet = document.querySelectorAll('.poins__violet')
+    pointsViolet.forEach(pointViolet => {
+        pointViolet.addEventListener("click", function (e) {
+            for (let i = 0; i < pointsViolet.length; i++) {
+                if (pointsViolet[i] == pointViolet) {
+                    positionViolet = -(widthSliderViolet * i)
+                    activeSlideViolet = i
+                    trackViolet.style.transform = `translateX(${positionViolet}px)`;
+                    setPointViolet();
+                }
+            }
+        })
+    });
 }
